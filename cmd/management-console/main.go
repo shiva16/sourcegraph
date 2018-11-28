89d53de0b58ed1f3ec86c9f3d871d4754f9b22bf
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-11-28 02:44:33 -0700

    mgmt console

--- cmd/management-console/main.go
+++ cmd/management-console/main.go
@@ -59,7 +59,9 @@ func main() {
 		log.Fatalf("Fatal error connecting to Postgres DB: %s", err)
 	}
 
-	// TODO: register HTTP handlers here!
+	mux := http.NewServeMux()
+	mux.HandleFunc("/get", serveCoreConfigurationGetLatest)
+	mux.HandleFunc("/create", serveCoreConfigurationCreateIfUpToDate)
 
 	host := ""
 	if env.InsecureDev {
@@ -68,4 +70,73 @@ func main() {
 	addr := net.JoinHostPort(host, port)
 	log15.Info("management-console: listening", "addr", addr)
 	log.Fatalf("Fatal error serving: %s", http.ListenAndServe(addr, nil))
+}
+
+func serveCoreConfigurationGetLatest(w http.ResponseWriter, r *http.Request) {
+	logger := log15.New("route", "serveCoreConfigurationGetLatest")
+
+	coreFile, err := dbHandler.CoreGetLatest(r.Context())
+	if err != nil {
+		logger.Error("dbHandler.CoreGetLatest failed", "error", err)
+		http.Error(w, "Unexpected error when retrieving latest core configuration file.", http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(coreFile)
+	if err != nil {
+		logger.Error("json response encoding failed", "error", err)
+		http.Error(w, "Unexpected error when encoding json response.", http.StatusInternalServerError)
+	}
+}
+
+type createIfUpToDateArgs struct {
+	LastID   *int32 `json:"lastID,omitempty"`
+	Contents string `json:"contents"`
+}
+
+func serveCoreConfigurationGetLatest(w http.ResponseWriter, r *http.Request) {
+	logger := log15.New("route", "serveCoreConfigurationGetLatest")
+
+	coreFile, err := dbHandler.CoreGetLatest(r.Context())
+	if err != nil {
+		logger.Error("dbHandler.CoreGetLatest failed", "error", err)
+		http.Error(w, "Unexpected error when retrieving latest core configuration file.", http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(coreFile)
+	if err != nil {
+		logger.Error("json response encoding failed", "error", err)
+		http.Error(w, "Unexpected error when encoding json response.", http.StatusInternalServerError)
+	}
+}
+
+func serveCoreConfigurationCreateIfUpToDate(w http.ResponseWriter, r *http.Request) {
+	logger := log15.New("route", "serveCoreConfigurationCreateIfUpToDate")
+
+	var args createIfUpToDateArgs
+	err := json.NewDecoder(r.Body).Decode(&args)
+	if err != nil {
+		logger.Error("json argument decoding failed", "error", err)
+		http.Error(w, "Unexpected error when decoding arguments.", http.StatusBadRequest)
+		return
+	}
+
+	coreFile, err := dbHandler.CoreCreateIfUpToDate(r.Context(), args.LastID, args.Contents)
+	if err != nil {
+		logger.Error("dbHandler.CoreCreateIfUpToDate failed", "error", err)
+		http.Error(w, "Unexpected error when updating latest core configuration file.", http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(coreFile)
+	if err != nil {
+		logger.Error("json response encoding failed", "error", err)
+		http.Error(w, "Unexpected error when encoding json response.", http.StatusInternalServerError)
+	}
+}
+
+type createIfUpToDateArgs struct {
+	LastID   *int32 `json:"lastID,omitempty"`
+	Contents string `json:"contents"`
 }
